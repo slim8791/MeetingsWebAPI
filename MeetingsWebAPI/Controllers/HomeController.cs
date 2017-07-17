70df09b54d7a64dc8b62e281c9093dbe6ff56aad
Slim HAMMAMI <slim.hammami.ext@easytek.tn>
2017-07-17 23:59:43 +0100

    first commit

--- MeetingsWebAPI/Controllers/HomeController.cs
+++ MeetingsWebAPI/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace MeetingsWebAPI.Controllers
 {
     public class HomeController : Controller
-    {
+    {//
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
